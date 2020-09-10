6a4839d787ced713fdf83c20f52123d473253474
Clayton Lewis <lewisclayton89@gmail.com>
2020-09-10 03:29:46 -0400

    its coming together

--- game-functions4.js
+++ game-functions4.js
@@ -9,6 +9,8 @@ const rap4 = document.querySelector('.rap4');
 
 let westAns = false;
 let actionAns = false;
+let larryAns = false;
+let asapAns = false;
 
 westside.onplay = () => {
 
@@ -19,6 +21,7 @@ westside.onplay = () => {
             westAns = true;
             rap1.style.backgroundColor = 'green';
             rap1.style.color = 'white';
+            rapTracker.push('Westside')
         }
         if (westAns === false) {
             target = event.target;
@@ -29,6 +32,47 @@ westside.onplay = () => {
     }
 }
 
+larry.onplay = () => {
+
+    spinWheel.onclick = event => {
+        let target = event.target.className;
+        if (target === 'rap2' || target === 'larryRap') {
+            console.log(target);
+            larryAns = true;
+            rap2.style.backgroundColor = 'green';
+            rap2.style.color = 'white';
+            rapTracker.push('Larry June')
+        }
+        if (larryAns === false) {
+            target = event.target;
+            console.log('wrong')
+            target.style.backgroundColor = 'red';
+            target.style.color = 'black';
+        }
+    }
+}
+
+asap.onplay = () => {
+
+    spinWheel.onclick = event => {
+        let target = event.target.className;
+        if (target === 'rap4' || target === 'asapRap') {
+            console.log(target);
+            asapAns = true;
+            rap4.style.backgroundColor = 'green';
+            rap4.style.color = 'white';
+            rapTracker.push('Asap Rocky')
+        }
+        if (asapAns === false) {
+            target = event.target;
+            console.log('wrong')
+            target.style.backgroundColor = 'red';
+            target.style.color = 'black';
+        }
+    }
+}
+
+
 action.onplay = () => {
 
     spinWheel.onclick = event => {
@@ -38,6 +82,7 @@ action.onplay = () => {
             actionAns = true;
             rap3.style.backgroundColor = 'green';
             rap3.style.color = 'white';
+            rapTracker.push('Action Bronson')
         }
         if (actionAns === false) {
             target = event.target;
@@ -48,15 +93,16 @@ action.onplay = () => {
     }
 }
 
-//westside.addEventListener('play', westSideRight);
-//larry.addEventListener('play', larryRight);
-//action.addEventListener('play', actionRight);
-//asap.addEventListener('play', asapRight);
+const rapTracker = [];
+console.log(rapTracker)
 
-/*
-spinWheel.onclick = function() {
-   document.querySelector('#spinner').classList.remove('spin-wheel');
-   document.querySelector('#spinner').classList.add('stop-spin');
+
+window.onclick = function () {
+    if (rapTracker.length === 4) {
+        document.querySelector('#spinner').classList.remove('spin-wheel');
+        document.querySelector('#spinner').classList.add('stop-spin');
+        console.log('you named all the rappers')
+        console.log(rapTracker)
+    }
 };
-*/
 
