5baeb65c08d534ead814baf628330172360ff319
Clayton Lewis <lewisclayton89@gmail.com>
2020-09-10 01:39:09 -0400

    making progress

--- game-functions4.js
+++ game-functions4.js
@@ -1,4 +1,4 @@
-import { westside, asap, action, larry } from './music-with-game4.js'
+import { westside, asap, action, larry } from './music-with-game4.js';
 
 let spinWheel = document.querySelector('#spinner');
 
@@ -7,87 +7,56 @@ const rap2 = document.querySelector('.rap2');
 const rap3 = document.querySelector('.rap3');
 const rap4 = document.querySelector('.rap4');
 
-let right = true;
-let answered = false
+let westAns = false;
+let actionAns = false;
 
-function westSideRight() {
+westside.onplay = () => {
 
-    console.log('westside is playing');
-
-    function westSpin(event) {
-        let target = event.target.id
-
-        if (target === 'westRap' && right === true || target === 'westRight' && right === true) {
-            rap1.classList.add('right')
-            answered = true
-        }  else if (target !== 'westRap' && answered === false || target !== 'westRight' && answered === false) {
+    spinWheel.onclick = event => {
+        let target = event.target.className;
+        if (target === 'rap1' || target === 'westRap') {
+            console.log(target);
+            westAns = true;
+            rap1.style.backgroundColor = 'green';
+            rap1.style.color = 'white';
+        }
+        if (westAns === false) {
             target = event.target;
-            target.classList.add('wrong')   
+            console.log('wrong')
+            target.style.backgroundColor = 'red';
+            target.style.color = 'black';
         }
     }
-    spinWheel.addEventListener('click', westSpin)
 }
 
-function larryRight() {
+action.onplay = () => {
 
-    console.log('larry is playing')
-
-    function larrySpin(event) {
-        let target = event.target.id
-        if (target === 'larryRap' && right === true || target === 'larryRight' && right === true) {
-            rap2.classList.add('right')
-            answered = true
-        } else if (target !== 'larryRap' && answered === false || target !== 'larryRight' && answered === false) {
+    spinWheel.onclick = event => {
+        let target = event.target.className;
+        if (target === 'rap3' || target === 'actionRap') {
+            console.log(target);
+            actionAns = true;
+            rap3.style.backgroundColor = 'green';
+            rap3.style.color = 'white';
+        }
+        if (actionAns === false) {
             target = event.target;
-            target.classList.add('wrong')
+            console.log('wrong')
+            target.style.backgroundColor = 'red';
+            target.style.color = 'black';
         }
     }
-    spinWheel.addEventListener('click', larrySpin)
-}
-/*
-function actionRight() {
-    console.log('action is playing')
-
-    function larrySpin(event) {
-        let target = event.target.id
-        if (target === 'actionRap' && rightAction  === true || target === 'actionRight' && rightAction === true) {
-            rap3.classList.add('right')
-            actionAns = true
-        } else if (target !== 'actionRap' && actionAns  === false || target !== 'actionRight' && actionAns  === false) {
-            target = event.target;
-            target.classList.add('wrong')
-        }
-    }
-    spinWheel.addEventListener('click', larrySpin)
 }
 
-
-function asapRight() {
-
-    console.log('asap is playing')
-
-    function asapSpin(event) {
-        let target = event.target.id
-        if (target === 'asapRap' && rightAsap === true || target === 'asapRight' && rightAsap === true) {
-            rap4.classList.add('right')
-            asapAns = true
-        } else if (target !== 'asapRap' && asapAns === false || target !== 'asapRight' && asapAns === false) {
-            target = event.target;
-            target.classList.add('wrong')
-        }
-    }
-    spinWheel.addEventListener('click', asapSpin)
-}
-*/
-
-westside.addEventListener('play', westSideRight)
-larry.addEventListener('play', larryRight)
-//action.addEventListener('play', actionRight)
-//asap.addEventListener('play', asapRight)
+//westside.addEventListener('play', westSideRight);
+//larry.addEventListener('play', larryRight);
+//action.addEventListener('play', actionRight);
+//asap.addEventListener('play', asapRight);
 
 /*
 spinWheel.onclick = function() {
-   document.querySelector('#spinner').classList.remove('spin-wheel')
-   document.querySelector('#spinner').classList.add('stop-spin')
-}
+   document.querySelector('#spinner').classList.remove('spin-wheel');
+   document.querySelector('#spinner').classList.add('stop-spin');
+};
 */
+
