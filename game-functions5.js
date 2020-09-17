0d8d1bd41da2a122372d5e40a3ffaae8f50de4f6
Clayton Lewis <lewisclayton89@gmail.com>
2020-09-16 22:36:02 -0400

    finishing up

--- game-functions5.js
+++ game-functions5.js
@@ -1,1 +1,44 @@
-console.log('YOU SEE ME NOW')
+
+let game = document.querySelector('.game-container')
+let musicPlaying = false
+
+let realTime = document.querySelector('.timer');
+let pause = document.querySelector('.pause');
+let start = document.querySelector('.start')
+let time = 60;
+
+function timer() {
+    
+
+    let counter = setInterval(() => {
+        if (musicPlaying === false) {
+            realTime.innerHTML = time;
+            time--
+            if( time <= -1) {
+                clearInterval(counter)
+                alert('you fucked up')
+            } 
+        }
+    }, 1000)
+  
+}
+
+//timer()
+pause.onclick = function() {
+    musicPlaying = false;
+    audio.pause()
+}
+
+start.onclick = function() {
+    musicPlaying = true;
+    audio.play()
+}
+
+let audio = document.getElementById('audio');
+
+audio.onplay = () => {
+    
+}
+
+timer()
+
